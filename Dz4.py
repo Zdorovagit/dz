3f7a5f65d7b0e2890b3e2977ba8a6870923e50ab
Zdorovagit <zdorova000@gmail.com>
2023-02-12 15:31:26 +0200

    Version 1.0

---
+++ Dz4.py
@@ -0,0 +1,18 @@
+import random
+
+class Human:
+    def __init__(self, name="Human"):
+        self.name = name
+        self.revolver = Revolver
+        self.alive = True
+
+    def game_death:
+        if Revolver == Rev1:
+
+class Revolver:
+    def __init__(self, revolver_list):
+        self.revolver = random.choice(list(revolver_list.values()))
+
+
+revolver_list = {'Rev1': {'patron': True},
+              'Rev2': {'patron': False}}
