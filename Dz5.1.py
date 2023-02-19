ab6fbdd42231c04bdb35c6d3c4fd7a57662bb92e
Zdorovagit <zdorova000@gmail.com>
2023-02-19 15:33:32 +0200

    Version 2.0

--- Dz5.1.py
+++ Dz5.1.py
@@ -2,22 +2,25 @@ result = []
 
 
 def divider(a, b):
-    if a < b:
-        raise ValueError
-    if b > 100:
-        raise IndexError
-    return a / b
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print('NO NULL')
+        return 0
+    except TypeError:
+        print('STR')
+        return 0
 
-try:
-    data = {10: 2,
-            2: 5,
-            123: 4,
-            18: 0,
-            '[]': 15,
-            8: 4}
-except:
-    for key in data:
-        res = divider(key, data[key])
-        result.append(res)
+
+data = {10: 2,
+        2: 5,
+        "123": 4,
+        18: 0,
+        '1': 15,
+        8: 4}
+
+for key in data:
+    res = divider(key, data[key])
+    result.append(res)
 
 print(result)
