a4bb86520e97fb04a02f08b4b0c530c3273b1d32
Zdorovagit <zdorova000@gmail.com>
2023-02-26 14:54:32 +0200

    Version 1.0

---
+++ Dz6.py
@@ -0,0 +1,24 @@
+def fibonacci():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+
+fib = fibonacci()
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
